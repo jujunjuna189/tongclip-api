2f3aa66f9d73efdb7fc56d88bdfe46ba6c6eae57
Ujun Junaedi <jujunjuna189@gmail.com>
2026-09-03 10:53:45 +0700

    Update cors

--- config/cors.php
+++ config/cors.php
@@ -6,6 +6,7 @@ return [
     'allowed_origins' => [
         env('FRONTEND_URL', 'http://127.0.0.1:5173'),
         'http://localhost:5173',
+        'https://tongclip.rssolusi.com',
     ],
     'allowed_origins_patterns' => [
         '#^http://localhost:517[3-9]$#',
