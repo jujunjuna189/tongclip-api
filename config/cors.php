398c4138462b929dc5df10e9803c11328cb54bb5
Ujun Junaedi <jujunjuna189@gmail.com>
2026-09-25 09:04:57 +0700

    Update

--- config/cors.php
+++ config/cors.php
@@ -3,15 +3,8 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-        env('FRONTEND_URL', 'http://127.0.0.1:5173'),
-        'http://localhost:5173',
-        'https://tongclip.rssolusi.com',
-    ],
-    'allowed_origins_patterns' => [
-        '#^http://localhost:517[3-9]$#',
-        '#^http://127\.0\.0\.1:517[3-9]$#',
-    ],
+    'allowed_origins' => ['*'],
+    'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
